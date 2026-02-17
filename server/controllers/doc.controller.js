8f2a71993ef61048fca584441281d711b4a4bf0f
Suprodip <suprodipbhattacharjee1@gmail.com>
2026-02-17 09:57:05 +0530

    completed the router of document model

---
+++ server/controllers/doc.controller.js
@@ -0,0 +1,9 @@
+export const handleGetAllDocs = async (req, res) => {};
+
+export const handleGetSingleDocs = async (req, res) => {};
+
+export const handleCreateDocs = async (req, res) => {};
+
+export const handleDeleteDocs = async (req, res) => {};
+
+export const handleUpdateDocs = async (req, res) => {};
---
+++ server/routes/doc.route.js
@@ -0,0 +1,11 @@
+import express from "express"
+import { handleCreateDocs, handleDeleteDocs, handleGetAllDocs, handleGetSingleDocs, handleUpdateDocs } from "../controllers/doc.controller.js";
+
+
+const router = express.Router();
+
+router.get("/all",handleGetAllDocs);
+router.get("/:id",handleGetSingleDocs);
+router.post("/create",handleCreateDocs);
+router.post("/update/:id",handleUpdateDocs);
+router.delete("/:id",handleDeleteDocs);
